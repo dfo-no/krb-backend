5bcff28a80ba4048752555172bb022906bc99603
fres <fres@computas.com>
2022-09-08 20:33:18 +0200

    rest assured integration test

--- src/main/kotlin/org/kravbank/repository/ProjectRepository.kt
+++ src/main/kotlin/org/kravbank/repository/ProjectRepository.kt
@@ -5,7 +5,8 @@ import org.kravbank.domain.ProjectKtl
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class ProjectRepository : PanacheRepository<ProjectKtl> {
+class ProjectRepository : PanacheRepository<ProjectKtl> { //project, long
+
     //fun findByTitle(name: String) = find("name", name).firstResult()
 
 }
--- src/test/kotlin/org/kravbank/GreetingResourceTest.kt
+++ src/test/kotlin/org/kravbank/GreetingResourceTest.kt
@@ -1,9 +1,18 @@
 package org.kravbank
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured
 import io.restassured.RestAssured.given
+import io.restassured.config.JsonConfig.jsonConfig
+import io.restassured.config.ObjectMapperConfig
+import io.restassured.config.RestAssuredConfig
+import io.restassured.parsing.Parser
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
+import org.kravbank.domain.ProjectKtl
 
 @QuarkusTest
 class GreetingResourceTest {
@@ -16,5 +25,32 @@ class GreetingResourceTest {
              .statusCode(200)
              .body(`is`("Hello RESTEasy"))
     }
+
+    @Test
+    fun createProject() {
+        RestAssured.defaultParser = Parser.JSON
+        RestAssured.baseURI = "http://localhost:8080";
+        //RestAssured.port = 8080;
+        RestAssured.basePath = "/kt";
+
+        val project = ProjectKtl()
+
+        project.description ="beskrivelse"
+        project.title = "Tittel"
+
+        given()
+            .`when`()
+            .body(project)
+            .header("Content-type", "application/json")
+            .post("/projects")
+            .then()
+            .statusCode(201) //envt 200
+        //body(both(startsWith("")).and(not(endsWith("null"))))
+
+
+    }
+
+
+
 
 }
