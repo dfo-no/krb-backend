0ff31ccafca06cfc169d808e3251bca8c3c2cffb
fres <fres@computas.com>
2022-10-12 15:11:42 +0200

    codelist resp

--- src/main/kotlin/org/kravbank/api/CodelistResource.kt
+++ src/main/kotlin/org/kravbank/api/CodelistResource.kt
@@ -3,7 +3,9 @@ package org.kravbank.api;
 import org.kravbank.utils.form.codelist.CodelistForm
 import org.kravbank.utils.form.codelist.CodelistFormUpdate
 import org.kravbank.service.CodelistService
-import org.kravbank.service.ProjectService
+import org.kravbank.utils.mapper.codelist.CodelistMapper
+import org.kravbank.utils.mapper.codelist.CodelistUpdateMapper
+import java.net.URI
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
@@ -12,39 +14,54 @@ import javax.ws.rs.core.MediaType.APPLICATION_JSON
 @Path("/api/v1/projects/{projectRef}/codelists")
 @Produces(APPLICATION_JSON)
 @Consumes(APPLICATION_JSON)
-class CodelistResource(val codelistService: CodelistService, val projectService: ProjectService) {
+class CodelistResource(val codelistService: CodelistService) {
     //GET ONE CODELIST
     @GET
     @Path("/{codelistRef}")
     fun getCodelistByRef(
         @PathParam("projectRef") projectRef: String,
         @PathParam("codelistRef") codelisRef: String
-    ): Response =
-        codelistService.get(projectRef, codelisRef)
+    ): Response {
+        val codelist = codelistService.get(projectRef, codelisRef)
+        //mapper fra entity
+        val codelistDTO = CodelistMapper().fromEntity(codelist)
+        return Response.ok(codelistDTO).build()
+    }
 
     //GET ALL CODELISTS
     @GET
     fun listCodelists(
         @PathParam("projectRef") projectRef: String
-    ): Response =
-        codelistService.list(projectRef)
+    ): Response {
+        val codelists = codelistService.list(projectRef)
+        val codelistsDTO = ArrayList<CodelistForm>()
+        //mapper fra entity
+        for (n in codelists) codelistsDTO.add(CodelistMapper().fromEntity(n))
+        return Response.ok(codelistsDTO).build()
+    }
 
     //CREATE CODELIST
     @Transactional
     @POST
     fun createCodelist(
-        @PathParam("projectRef") projectRef: String, codelist: CodelistForm
-    ): Response =
-        codelistService.create(projectRef, codelist)
+        @PathParam("projectRef") projectRef: String, codelistDTO: CodelistForm
+    ): Response {
+        val codelist = codelistService.create(projectRef, codelistDTO)
+        //sender ny codelist ref i response header
+        return Response.created(URI.create("/api/v1/projects/$projectRef/codelists/" + codelist.ref)).build()
+    }
 
     //DELETE CODELIST
     @DELETE
     @Path("{codelistRef}")
     @Transactional
-    fun deleteCodelistById(
+    fun deleteCodelist(
         @PathParam("projectRef") projectRef: String,
         @PathParam("codelistRef") codelistRef: String
-    ): Response = codelistService.delete(projectRef, codelistRef)
+    ): Response  {
+        codelistService.delete(projectRef, codelistRef)
+        return Response.noContent().build()
+    }
 
     //UPDATE CODELIST
     @PUT
@@ -54,5 +71,12 @@ class CodelistResource(val codelistService: CodelistService, val projectService:
         @PathParam("projectRef") projectRef: String,
         @PathParam("codelistRef") codelistRef: String,
         updateCodelist: CodelistFormUpdate
-    ): Response = codelistService.update(projectRef, codelistRef, updateCodelist)
+    ): Response {
+        val codelist = codelistService.update(projectRef, codelistRef, updateCodelist)
+        // mapper fra entity
+        //
+        //
+        val codelistUpdateDTO = CodelistUpdateMapper().fromEntity(codelist)
+        return Response.ok(codelistUpdateDTO).build()
+    }
 }
