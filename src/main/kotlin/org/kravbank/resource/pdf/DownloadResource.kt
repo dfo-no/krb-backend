6a9d468c4dfb212c9f435a21dfba4f16a21deb97
fres <fres@computas.com>
2022-11-20 10:12:08 +0100

    Add headers

--- src/main/kotlin/org/kravbank/resource/pdf/DownloadResource.kt
+++ src/main/kotlin/org/kravbank/resource/pdf/DownloadResource.kt
@@ -2,7 +2,6 @@ package org.kravbank.resource.pdf
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.itextpdf.text.pdf.codec.Base64
-import io.vertx.mutiny.core.http.HttpHeaders.headers
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody
 import org.jboss.resteasy.annotations.cache.NoCache
 import org.kravbank.service.pdf.PdfService
@@ -11,7 +10,6 @@ import javax.ws.rs.Consumes
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
-import javax.ws.rs.core.HttpHeaders
 import javax.ws.rs.core.MediaType.APPLICATION_JSON
 import javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM
 import javax.ws.rs.core.Response
@@ -22,35 +20,18 @@ class DownloadResource {
     @POST
     @Path(value = "/generateSpecification")
     @Consumes(APPLICATION_JSON)
-    @Produces(APPLICATION_OCTET_STREAM)
+    @Produces(APPLICATION_OCTET_STREAM) //PDF
     @NoCache
     fun generateSpecification(@RequestBody jsonNode: JsonNode): Response {
         val bis: ByteArrayInputStream = PdfService.generateSpecification(jsonNode)
 
-        /**
-         * Todo: Utkommentert kode er fra den originale java-filen. Slettes hvis migrering ok.
-        var headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf");
-        headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE);
-        headers.setCacheControl(CacheControl.noCache());
-        headers.add(HttpHeaders.PRAGMA, "public");
-        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
-        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF)
-        .body(new InputStreamResource(bis));
-         *
-         */
-
-        /*
-                val header = HashMap<String, String>()
-                header[HttpHeaders.CONTENT_DISPOSITION] = "attachment; filename=report.pdf"
-                header[HttpHeaders.ALLOW] = "*"
-                headers().addAll(header)
-         */
-
-        headers().add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
-        headers().add(HttpHeaders.ALLOW, "*")
-
-        return Response.ok(Base64.InputStream(bis)).build() //todo: blir headers satt i Responsen?
+        return Response
+            .ok(Base64.InputStream(bis))
+            .header("CONTENT_DISPOSITION", "attachment; filename=report.pdf") // Headers for Swagger UI.
+            .header("CONTENT_TYPE", "APPLICATION_PDF_VALUE")
+            .header("PRAGMA", "public")
+            .header("ACCESS_CONTROL_ALLOW_ORIGIN", "*")
+            .build()
     }
 
     @POST
@@ -60,9 +41,13 @@ class DownloadResource {
     @NoCache
     fun generateResponse(@RequestBody jsonNode: JsonNode): Response {
         val bis: ByteArrayInputStream = PdfService.generateResponse(jsonNode)
-        headers().add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
-        headers().add(HttpHeaders.ALLOW, "*")
-        return Response.ok(Base64.InputStream(bis)).build()
+        return Response
+            .ok(Base64.InputStream(bis))
+            .header("CONTENT_DISPOSITION", "attachment; filename=report.pdf") // Headers for Swagger UI.
+            .header("CONTENT_TYPE", "APPLICATION_PDF_VALUE")
+            .header("PRAGMA", "public")
+            .header("ACCESS_CONTROL_ALLOW_ORIGIN", "*")
+            .build()
     }
 
     @POST
@@ -72,8 +57,26 @@ class DownloadResource {
     @NoCache
     fun generatePrefilledResponse(@RequestBody jsonNode: JsonNode): Response {
         val bis: ByteArrayInputStream = PdfService.generatePrefilledResponse(jsonNode)
-        headers().add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
-        headers().add(HttpHeaders.ALLOW, "*")
-        return Response.ok(Base64.InputStream(bis)).build()
+        return Response
+            .ok(Base64.InputStream(bis))
+            .header("CONTENT_DISPOSITION", "attachment; filename=report.pdf") // Headers for Swagger UI.
+            .header("CONTENT_TYPE", "APPLICATION_PDF_VALUE")
+            .header("PRAGMA", "public")
+            .header("ACCESS_CONTROL_ALLOW_ORIGIN", "*")
+            .build()
     }
 }
+
+
+/**
+ * Todo: Utkommentert kode er fra den originale java-filen. Slettes hvis migrering ok.
+var headers = new HttpHeaders();
+headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf");
+headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE);
+headers.setCacheControl(CacheControl.noCache());
+headers.add(HttpHeaders.PRAGMA, "public");
+headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF)
+.body(new InputStreamResource(bis));
+ *
+ */
