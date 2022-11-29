c65ff5eaad0f1ff0c6d0d1b37d2aee584419da0f
Fredrik Sandvold <fres@computas.com>
2022-11-29 14:25:45 +0100

    More setup

--- src/test/kotlin/org/kravbank/TestSetup.kt
+++ src/test/kotlin/org/kravbank/TestSetup.kt
@@ -1,5 +1,6 @@
 package org.kravbank
 
+import org.kravbank.dao.NeedForm
 import org.kravbank.dao.ProjectForm
 import org.kravbank.dao.PublicationForm
 import org.kravbank.domain.*
@@ -28,13 +29,22 @@ class TestSetup {
         var publications: MutableList<Publication> = mutableListOf()
         var products: MutableList<Product> = mutableListOf()
 
+        lateinit var newPublication: Publication
+
         lateinit var projectForm: ProjectForm
 
         lateinit var publicationForm: PublicationForm
 
+        lateinit var needForm: NeedForm
+
+        lateinit var newNeed: Need
+
         lateinit var updatedProjectForm: ProjectForm
 
         lateinit var updatedPublicationForm: PublicationForm
+
+        lateinit var updatedNeedForm: NeedForm
+
 
         val dateTime: LocalDateTime = LocalDateTime.of(2021, 2, 21, 10, 10, 10)
 
@@ -47,7 +57,7 @@ class TestSetup {
         val need_projectRef: String = "need1b69-edb2-431f-855a-4368e2bcddd1"
 
         fun arrange() {
-            //DOMAIN
+
             project.title = "første prosjekt"
             project.description = "første prosjektbeskrivelse"
             project.ref = "ccc4db69-edb2-431f-855a-4368e2bcddd1"
@@ -81,7 +91,7 @@ class TestSetup {
             need = Need()
             need.ref = "ewdsfsada567"
             need.id = 122L
-            need.title = "tittel"
+            need.title = "Ny need tittel"
             need.description = "desv"
             need.requirements = requirements
             need.project = project
@@ -92,10 +102,10 @@ class TestSetup {
             publication.ref = "zzz4db69-edb2-431f-855a-4368e2bcddd1"
             publication.project = project
             publication.date = dateTime
-            publication.comment = "En publicationskommentar"
+            publication.comment = "En til kommentar"
+            publication.version = 10
 
             requirement = Requirement()
-            projects.add(project)
 
             val newProject = Project()
             newProject.title = "andre prosjekt"
@@ -108,28 +118,48 @@ class TestSetup {
             newProject.needs = needs
             newProject.products = products
 
-            projectForm = ProjectForm().fromEntity(newProject)
 
-            val newPublication = Publication()
+            newPublication = Publication()
             newPublication.id = 201
             newPublication.ref = "asdsa-fdsf-34-fsd-dsgf-35463fd"
             newPublication.project = project
-            newPublication.date = dateTime
-            newPublication.comment = "En ny publicationskommentar"
+            //newPublication.date = dateTime
+            newPublication.comment = "En ny kommentar"
 
+
+            newNeed = Need()
+            newNeed.ref = "3565-245tfdsf43-r4wet3435"
+            newNeed.id = 123L
+            newNeed.title = "Ny need tittel form"
+            newNeed.description = "need beskrivelse"
+            newNeed.requirements = requirements
+            newNeed.project = project
+
+            //Forms
+            projectForm = ProjectForm().fromEntity(newProject)
             publicationForm = PublicationForm().fromEntity(newPublication)
+            needForm = NeedForm().fromEntity(newNeed)
 
-            //FORM Update Project
+            //update
+
             updatedProjectForm = ProjectForm()
             updatedProjectForm.title = "Oppdatert tittel"
             updatedProjectForm.description = "Oppdatert beskrivelse"
 
-
-            //FORM Update Project
             updatedPublicationForm = PublicationForm()
-            updatedPublicationForm.ref = "zzz4db69-edb2-431f-855a-4368e2bcddd1"
             updatedPublicationForm.comment = "Oppdatert tittel"
-            updatedPublicationForm.date = LocalDateTime.of(2018, 2, 21, 10, 10, 10)
+
+            updatedNeedForm = NeedForm()
+            updatedNeedForm.title = "Endre tittel som need"
+            updatedNeedForm.description = "Endre beskrivelse som need"
+
+            //Adder til listene
+            projects.add(project)
+            projects.add(newProject)
+            needs.add(need)
+            publications.add(publication)
+            publications.add(newPublication)
+
         }
     }
 }
