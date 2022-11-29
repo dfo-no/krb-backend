95f892b470a04903a76699afc9e072e9eada3dd1
Fredrik Sandvold <fres@computas.com>
2022-11-29 14:25:22 +0100

    Need service test

--- src/test/kotlin/org/kravbank/service/NeedServiceTest.kt
+++ src/test/kotlin/org/kravbank/service/NeedServiceTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test
 import org.kravbank.TestSetup
 import org.kravbank.domain.Need
 import org.kravbank.repository.NeedRepository
+import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
 import javax.inject.Inject
 
@@ -33,8 +34,7 @@ internal class NeedServiceTest {
             .`when`(
                 needRepository
                     .findByRef(setup.project_needId, setup.need_projectRef)
-            )
-            .thenReturn(setup.need)
+            ).thenReturn(setup.need)
 
         val mockedNeed: Need =
             needService.get(setup.project_needRef, setup.need_projectRef)
@@ -47,7 +47,6 @@ internal class NeedServiceTest {
 
     @Test
     fun list() {
-
         Mockito.`when`(
             needRepository
                 .listAllNeeds(setup.project_needId)
@@ -57,7 +56,7 @@ internal class NeedServiceTest {
 
         Assertions.assertEquals(setup.need.title, mockedNeeds[0].title)
         Assertions.assertEquals(setup.need.description, mockedNeeds[0].description)
-        //Assertions.assertEquals(setup.project.ref, mockedNeeds[0].ref)
+        Assertions.assertEquals(setup.need.ref, mockedNeeds[0].ref)
         Assertions.assertEquals(setup.need.requirements, mockedNeeds[0].requirements)
         Assertions.assertEquals(setup.need.project, mockedNeeds[0].project)
         Assertions.assertEquals(setup.project.requirements, mockedNeeds[0].requirements)
@@ -65,13 +64,44 @@ internal class NeedServiceTest {
 
     @Test
     fun create() {
+        Mockito
+            .doNothing()
+            .`when`(needRepository)
+            .persist(ArgumentMatchers.any(Need::class.java))
+        Mockito
+            .`when`(needRepository.isPersistent(ArgumentMatchers.any(Need::class.java)))
+            .thenReturn(true)
+
+        val mockedNeed: Need =
+            needService.create(setup.need.project!!.ref, setup.needForm)
+
+        Assertions.assertNotNull(mockedNeed)
+        Assertions.assertEquals(setup.newNeed.title, mockedNeed.title)
+        Assertions.assertEquals(setup.newNeed.description, mockedNeed.description)
+        Assertions.assertEquals(setup.newNeed.requirements, mockedNeed.requirements)
     }
 
     @Test
     fun delete() {
+        //todo: Kommer tilbake til denne testen senere
     }
 
     @Test
     fun update() {
+        Mockito
+            .`when`(
+                needRepository
+                    .findByRef(setup.project_needId, setup.need_projectRef)
+            ).thenReturn(setup.need)
+
+        val mockedNeed: Need = needService.update(
+            setup.project_needRef,
+            setup.need_projectRef,
+            setup.updatedNeedForm
+        )
+
+        Assertions.assertNotNull(mockedNeed)
+        Assertions.assertEquals(setup.updatedNeedForm.title, mockedNeed.title)
+        Assertions.assertEquals(setup.updatedNeedForm.description, mockedNeed.description)
     }
 }
