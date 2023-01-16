4d15b40a9b5283a4e8214d3f2385b6423e60b662
Fredrik Sandvold <fres@computas.com>
2023-01-16 14:38:53 +0100

    Date is only set in SQL trigger function

--- src/main/kotlin/org/kravbank/domain/DeleteRecord.kt
+++ src/main/kotlin/org/kravbank/domain/DeleteRecord.kt
@@ -2,7 +2,6 @@ package org.kravbank.domain
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase
 import org.hibernate.annotations.CreationTimestamp
-import java.sql.Timestamp
 import java.time.LocalDateTime
 import java.util.*
 import javax.persistence.*
@@ -21,7 +20,7 @@ data class DeleteRecord(
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "deletedAt", nullable = false)
-    var deletedAt: Date? = Timestamp.valueOf(LocalDateTime.now()),
+    var deletedAt: Date? = null,
 
     var updatedAt: LocalDateTime? = null,
 
@@ -35,9 +34,4 @@ data class DeleteRecord(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
-
-    @Override
-    override fun toString(): String {
-        return this::class.simpleName + "(id = $id )"
-    }
 }
