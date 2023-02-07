c9e584a6238a5f733843662fc15d9211aeacb506
Fredrik Sandvold <fres@computas.com>
2023-02-07 21:22:43 +0100

    just another root

---
+++ src/main/kotlin/org/kravbank/resource/RootResource.kt
@@ -0,0 +1,14 @@
+package org.kravbank.resource
+
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.core.Response
+
+@Path("/")
+class RootResource {
+
+    @GET
+    fun get(): Response {
+        return Response.ok("Vælkømmin").build()
+    }
+}
