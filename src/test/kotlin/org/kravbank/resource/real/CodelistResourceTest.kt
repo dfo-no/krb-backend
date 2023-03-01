97530f7620f4f20dfc2db7c217022e002211a8df
Fredrik Sandvold <111272438+fres00@users.noreply.github.com>
2023-03-01 10:57:34 +0100

    Isolated integration tests. More precise function names

--- src/test/kotlin/org/kravbank/resource/real/CodelistResourceTest.kt
+++ src/test/kotlin/org/kravbank/resource/real/CodelistResourceTest.kt
@@ -1,94 +1,194 @@
-package org.kravbank.resource.real
+package org.kravbank.resource
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.parsing.Parser
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.kravbank.dao.CodelistForm
+import org.kravbank.domain.*
+import org.kravbank.repository.ProjectRepository
 import org.kravbank.utils.KeycloakAccess
+import java.util.*
+import javax.inject.Inject
+import javax.transaction.Transactional
 
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @QuarkusTest
 class CodelistResourceTest {
 
-    private val token = KeycloakAccess.getAccessToken("bob")
+    lateinit var token: String
 
-    @Test
-    fun `get codelist then return 200`() {
-        given()
-            .auth()
-            .oauth2(token)
-            .`when`()
-            .get("/api/v1/projects/bbb4db69-edb2-431f-855a-4368e2bcddd1/codelists/qqq4db69-edb2-431f-855a-4368e2bcddd1")
-            .then()
-            .statusCode(200)
+    lateinit var projectRef: String
+
+    var projectId: Long? = null
+
+    lateinit var newCodelistRef: String
+
+
+    @Inject
+    lateinit var projectRepository: ProjectRepository
+
+    private val mockedProducts: MutableList<Product> = mutableListOf()
+
+    private val mockedPublications: MutableList<Publication> = mutableListOf()
+
+    private val mockedRequirements: MutableList<Requirement> = mutableListOf()
+
+    private val mockedNeeds: MutableList<Need> = mutableListOf()
+
+    private val mockedCodelists: MutableList<Codelist> = mutableListOf()
+
+
+    @BeforeAll
+    @Transactional
+    fun `set up`() {
+
+        token = KeycloakAccess.getAccessToken("alice")
+
+        Project().apply {
+            ref = "testref-123"
+            title = "Prosjekttittel1"
+            description = "Prosjektbeskrivelse1"
+            products = mockedProducts
+            publications = mockedPublications
+            requirements = mockedRequirements
+            needs = mockedNeeds
+            codelist = mockedCodelists
+        }.run {
+            projectRepository.persistAndFlush(this)
+            projectRef = ref
+            projectId = id
+        }
     }
 
-    @Test
-    fun `list codelist then return 200`() {
-        given()
-            .auth()
-            .oauth2(token)
-            .`when`()
-            .get("/api/v1/projects/bbb4db69-edb2-431f-855a-4368e2bcddd1/codelists/")
-            .then()
-            .statusCode(200)
-    }
 
     @Test
-    fun `create codelist then return 201`() {
+    @Order(1)
+    fun `create codelist then return 201 and verify ref has value`() {
 
         RestAssured.defaultParser = Parser.JSON
 
-        val codelist = CodelistForm().apply {
-            title = "CODELIST Integrasjonstest - Tittel 1"
-            description = "CODELIST Integrasjonstest - Beskrivelse 1"
-        }.run {
-            CodelistForm().toEntity(this)
+        val createForm = CodelistForm().apply {
+            title = "Codeform from integration test"
+            description = "Codeform says hello"
         }
 
-        given()
+        val response =
+            given()
+                .auth()
+                .oauth2(token)
+                .`when`()
+                .body(CodelistForm().toEntity(createForm))
+                .header("Content-type", "application/json")
+                .post("/api/v1/projects/$projectRef/codelists")
+
+        assertEquals(201, response.statusCode)
+        assertTrue(response.headers.hasHeaderWithName("Location"))
+
+
+        val newUrlLocation = response.headers.getValue("Location")
+        val newRefToUse = newUrlLocation.split("codelists/")[1]
+
+        assertTrue(newRefToUse.isNotEmpty())
+
+        newCodelistRef = newRefToUse
+    }
+
+
+    @Order(2)
+    @Test
+    fun `get codelist then return 200 and verify ref`() {
+
+        val response = given()
             .auth()
             .oauth2(token)
             .`when`()
-            .body(codelist)
-            .header("Content-type", "application/json")
-            .post("/api/v1/projects/bbb4db69-edb2-431f-855a-4368e2bcddd1/codelists")
-            .then()
-            .statusCode(201)
+            .get("/api/v1/projects/$projectRef/codelists/$newCodelistRef")
+
+        assertEquals(200, response.statusCode())
+
+
+        val entity = response.body.jsonPath().getObject("", Codelist::class.java)
+
+        assertEquals(newCodelistRef, entity.ref)
     }
 
+
     @Test
-    fun `delete codelist then return 204`() {
-        given()
+    @Order(3)
+    fun `list codelist then return 200 and verify ref`() {
+
+        val response = given()
             .auth()
             .oauth2(token)
             .`when`()
-            .delete("/api/v1/projects/prosjekt5-edb2-431f-855a-4368e2bcddd1/codelists/newlist2222db69-edb2-431f-855a-4368e2bcddd1")
-            .then()
-            .statusCode(204)
+            .get("/api/v1/projects/$projectRef/codelists/")
+
+        assertEquals(200, response.statusCode)
+
+
+        val listOfCodelist = response.body.jsonPath().getList("", Codelist::class.java)
+        val lastEntityInList = listOfCodelist.last()
+
+
+        assertEquals(newCodelistRef, lastEntityInList.ref)
     }
 
 
     @Test
-    fun `update codelist then return 200`() {
-        RestAssured.defaultParser = Parser.JSON
+    @Order(4)
+    fun `update codelist then return 200 and verify updated attributes`() {
 
-        val codelist = CodelistForm().apply {
-            title = "CODELIST Oppdatert integrasjonstest - Tittel 1"
-            description = "CODELIST Oppdatert integrasjonstest - Beskrivelse 1"
-        }.run {
-            CodelistForm().toEntity(this)
+        val updateForm = CodelistForm().apply {
+            title = "Endrer tittel"
+            description = "Endrer beskrivelse"
         }
 
-        given()
+        val response = given()
             .auth()
             .oauth2(token)
             .`when`()
-            .body(codelist)
+            .body(CodelistForm().toEntity(updateForm))
             .header("Content-type", "application/json")
-            .put("/api/v1/projects/bbb4db69-edb2-431f-855a-4368e2bcddd1/codelists/qqq4db69-edb2-431f-855a-4368e2bcddd1")
-            .then()
-            .statusCode(200)
+            .put("/api/v1/projects/$projectRef/codelists/$newCodelistRef")
+
+        assertEquals(200, response.statusCode)
+
+
+        val entity = response.body.jsonPath().getObject("", Codelist::class.java)
+
+        assertEquals(updateForm.title, entity.title)
+        assertEquals(updateForm.description, entity.description)
     }
+
+
+    @Order(5)
+    @Test
+    fun `delete codelist then return 200 and verify deleted ref`() {
+
+        val response = given()
+            .auth()
+            .oauth2(token)
+            .`when`()
+            .delete("/api/v1/projects/$projectRef/codelists/$newCodelistRef")
+
+        assertEquals(200, response.statusCode)
+
+
+        val deletedRefConfirm = response.body.asPrettyString()
+        assertEquals(newCodelistRef, deletedRefConfirm)
+    }
+
+    @AfterAll
+    @Transactional
+    fun `delete dependency project`() {
+
+        projectRepository.deleteById(projectId)
+    }
+
 }
