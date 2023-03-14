d79260c2855c80c2cb8a75b830f733625d8db682
Fredrik Sandvold <111272438+fres00@users.noreply.github.com>
2023-03-14 12:45:30 +0100

    remove variant field

--- src/main/kotlin/org/kravbank/service/ProductService.kt
+++ src/main/kotlin/org/kravbank/service/ProductService.kt
@@ -6,7 +6,6 @@ import org.kravbank.lang.BackendException
 import org.kravbank.lang.BadRequestException
 import org.kravbank.repository.ProductRepository
 import org.kravbank.repository.ProjectRepository
-import org.kravbank.repository.RequirementVariantRepository
 import org.kravbank.utils.Messages.RepoErrorMsg.PRODUCT_BADREQUEST_CREATE
 import javax.enterprise.context.ApplicationScoped
 
@@ -15,9 +14,7 @@ import javax.enterprise.context.ApplicationScoped
 class ProductService(
     val productRepository: ProductRepository,
     val projectRepository: ProjectRepository,
-    val requirementVariantRepository: RequirementVariantRepository,
 ) {
-
 
     fun get(projectRef: String, productRef: String): Product {
         val foundProject = projectRepository.findByRef(projectRef)
@@ -29,26 +26,36 @@ class ProductService(
         return productRepository.listAllProducts(foundProject.id)
     }
 
+
     @Throws(BackendException::class)
     fun create(projectRef: String, newProduct: ProductForm): Product {
+
         val foundProject = projectRepository.findByRef(projectRef)
-        val foundReqVariant = requirementVariantRepository.findByRefProduct(newProduct.requirementVariantRef)
 
-        val product = ProductForm().toEntity(newProduct)
-        product.project = foundProject
-        product.requirementvariant = foundReqVariant
-
-        productRepository.persistAndFlush(product)
-        if (!productRepository.isPersistent(product)) throw BadRequestException(PRODUCT_BADREQUEST_CREATE)
-
-        return product
+        return ProductForm().toEntity(newProduct).apply {
+            project = foundProject
+        }.also {
+            productRepository.persistAndFlush(it)
+            if (!productRepository.isPersistent(it)) throw BadRequestException(PRODUCT_BADREQUEST_CREATE)
+        }
     }
-
 
     fun delete(projectRef: String, productRef: String): Product {
         val foundProject = projectRepository.findByRef(projectRef)
         val foundProduct = productRepository.findByRef(foundProject.id, productRef)
 
+        //TODO product in variants?
+//        val productUsedInVariant =
+//            requirementVariantRepository.listAllRequirementVariants(foundProject.id)
+//                .map { p -> p.product }
+//                .any { p -> p?.any()?.equals(foundProduct) == true }
+//
+//        if (productUsedInVariant) throw BackendException("Cannot delete. It is in use elsewhere")
+
+
+        // TODO
+        // pga deleteById bulksletter uavhengig av regler og relasjoner er det greit å sjekke om produkt inngår i varianter
+        // samme med prosjekter ?
         productRepository.deleteById(foundProduct.id)
 
         return foundProduct
