5414446dcfb96c7e2637f7a3ab915b0168a8de84
Fredrik Sandvold <111272438+fres00@users.noreply.github.com>
2023-03-17 20:04:16 +0100

    A litte more extensive refactor with need in scope + isolated integration tests

--- src/test/kotlin/org/kravbank/resource/real/RequirementResourceTest.kt
+++ src/test/kotlin/org/kravbank/resource/real/RequirementResourceTest.kt
@@ -4,84 +4,204 @@ import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.parsing.Parser
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.kravbank.dao.RequirementForm
+import org.kravbank.domain.*
+import org.kravbank.repository.NeedRepository
+import org.kravbank.repository.ProjectRepository
 import org.kravbank.utils.KeycloakAccess
+import javax.inject.Inject
+import javax.transaction.Transactional
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @QuarkusTest
 class RequirementResourceTest {
 
-    private val token = KeycloakAccess.getAccessToken("alice")
+    lateinit var token: String
+
+    lateinit var projectRef: String
+
+    var projectId: Long? = null
+
+    var needId: Long? = null
+
+    var needRef: String? = null
+
+    lateinit var newRequirementRef: String
 
 
-    @Test
-    fun getRequirement() {
-        given()
-            .auth()
-            .oauth2(token)
-            .`when`()
-            .get("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements/reqd2b69-edb2-431f-855a-4368e2bcddd1")
-            .then()
-            .statusCode(200)
+    @Inject
+    lateinit var projectRepository: ProjectRepository
+
+    @Inject
+    lateinit var needRepository: NeedRepository
+
+    private val mockedProducts: MutableList<Product> = mutableListOf()
+
+    private val mockedPublications: MutableList<Publication> = mutableListOf()
+
+    private val mockedRequirements: MutableList<Requirement> = mutableListOf()
+
+    private val mockedNeeds: MutableList<Need> = mutableListOf()
+
+    private val mockedCodelists: MutableList<Codelist> = mutableListOf()
+
+
+    @BeforeAll
+    @Transactional
+    fun setUp() {
+
+        token = KeycloakAccess.getAccessToken("alice")
+
+        val useThisProject = Project().apply {
+            ref = "testref-123"
+            title = "Prosjekttittel1"
+            description = "Prosjektbeskrivelse1"
+            products = mockedProducts
+            publications = mockedPublications
+            requirements = mockedRequirements
+            needs = mockedNeeds
+            codelist = mockedCodelists
+        }.also {
+            projectRepository.persistAndFlush(it)
+            projectRef = it.ref
+            projectId = it.id
+        }
+
+        Need().apply {
+            ref = "ytuiop-rtgsd-235235235-fdsfdsd"
+            title = "Ny need tittel"
+            project = useThisProject
+            requirements = mockedRequirements
+        }.also {
+            needRepository.persistAndFlush(it)
+            needRef = it.ref
+            needId = it.id
+        }
     }
 
-    @Test
-    fun listRequirements() {
-        given()
-            .auth()
-            .oauth2(token)
-            .`when`()
-            .get("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements/")
-            .then()
-            .statusCode(200)
-    }
 
     @Test
-    fun createRequirement() {
+    @Order(1)
+    fun `create requirement then return 201 and verify ref has value`() {
+
         RestAssured.defaultParser = Parser.JSON
-        val form = RequirementForm().apply {
+
+        val create = RequirementForm().apply {
+            title = "Ny requirement"
+            description = "Beskrivelse requirement"
+        }
+
+        val response =
+            given()
+                .auth()
+                .oauth2(token)
+                .`when`()
+                .body(RequirementForm().toEntity(create))
+                .header("Content-type", "application/json")
+                .post("/api/v1/projects/$projectRef/needs/$needRef/requirements")
+
+        Assertions.assertEquals(201, response.statusCode)
+        Assertions.assertTrue(response.headers.hasHeaderWithName("Location"))
+
+
+        val newUrlLocation = response.headers.getValue("Location")
+        val newRefToUse = newUrlLocation.split("requirements/")[1]
+
+        Assertions.assertTrue(newRefToUse.isNotEmpty())
+
+        newRequirementRef = newRefToUse
+    }
+
+
+    @Order(2)
+    @Test
+    fun `get requirement then return 200 and verify ref`() {
+
+        val response = given()
+            .auth()
+            .oauth2(token)
+            .`when`()
+            .get("/api/v1/projects/$projectRef/needs/$needRef/requirements/$newRequirementRef")
+
+        Assertions.assertEquals(200, response.statusCode())
+
+        val entity = response.body.jsonPath().getObject("", RequirementVariant::class.java)
+
+        Assertions.assertEquals(newRequirementRef, entity.ref)
+    }
+
+    @Test
+    @Order(3)
+    fun `list requirements then return 200 and verify ref`() {
+
+        val response = given()
+            .auth()
+            .oauth2(token)
+            .`when`()
+            .get("/api/v1/projects/$projectRef/needs/$needRef/requirements")
+
+        Assertions.assertEquals(200, response.statusCode)
+
+        println(response.body.peek())
+
+        val list = response.body.jsonPath().getList("", Requirement::class.java)
+
+        val lastEntityInList = list.last()
+
+
+        Assertions.assertEquals(newRequirementRef, lastEntityInList.ref)
+    }
+
+    @Test
+    @Order(4)
+    fun `update requirement then return 200 and verify updated attributes`() {
+
+        RestAssured.defaultParser = Parser.JSON
+
+        val update = RequirementForm().apply {
             title = "Integrasjonstest requirement - tittel 1"
             description = "Integrasjonstest requirement - beskrivelse 1"
         }
 
-        given()
+        val response = given()
             .auth()
             .oauth2(token)
             .`when`()
-            .body(form)
+            .body(RequirementForm().toEntity(update))
             .header("Content-type", "application/json")
-            .post("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements")
-            .then()
-            .statusCode(201)
+            .put("/api/v1/projects/$projectRef/needs/$needRef/requirements/$newRequirementRef")
+
+        Assertions.assertEquals(200, response.statusCode)
+
+
+        val entity = response.body.jsonPath().getObject("", Requirement::class.java)
+
+        Assertions.assertEquals(update.title, entity.title)
+        Assertions.assertEquals(update.description, entity.description)
     }
 
+
+    @Order(5)
     @Test
-    fun updateRequirement() {
-        RestAssured.defaultParser = Parser.JSON
-        val form = RequirementForm()
-        form.title = "Integrasjonstest requirement - tittel 1"
-        form.description = "Integrasjonstest requirement - beskrivelse 1"
+    fun `delete requirement then return 200 and verify deleted ref`() {
 
-
-        given()
+        val response = given()
             .auth()
             .oauth2(token)
             .`when`()
-            .body(form)
-            .header("Content-type", "application/json")
-            .put("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements/req1b69-edb2-431f-855a-4368e2bcddd1")
-            .then()
-            .statusCode(200)
+            .delete("/api/v1/projects/$projectRef/needs/$needRef/requirements/$newRequirementRef")
+
+        Assertions.assertEquals(200, response.statusCode)
     }
 
-    @Test
-    fun deleteRequirement() {
-        given()
-            .auth()
-            .oauth2(token)
-            .`when`()
-            .delete("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements/reqd2b69-edb2-431f-855a-4368e2bcddd1")
-            .then()
-            .statusCode(200)
+
+    @AfterAll
+    @Transactional
+    fun `delete dependencies`() {
+
+        projectRepository.deleteById(projectId)
+        needRepository.deleteById(needId)
     }
 }
--- src/test/kotlin/org/kravbank/resource/real/RequirementVariantResourceTest.kt
+++ src/test/kotlin/org/kravbank/resource/real/RequirementVariantResourceTest.kt
@@ -4,96 +4,239 @@ import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.parsing.Parser
-import org.junit.jupiter.api.Order
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.kravbank.dao.RequirementVariantForm
+import org.kravbank.domain.*
+import org.kravbank.repository.NeedRepository
+import org.kravbank.repository.ProjectRepository
+import org.kravbank.repository.RequirementRepository
 import org.kravbank.utils.KeycloakAccess
+import javax.inject.Inject
+import javax.transaction.Transactional
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @QuarkusTest
 class RequirementVariantResourceTest {
 
-    private val token = KeycloakAccess.getAccessToken("alice")
+    lateinit var token: String
+
+    lateinit var projectRef: String
+
+    var projectId: Long? = null
+
+    var needId: Long? = null
+
+    var needRef: String? = null
+
+    var requirementId: Long? = null
+
+    lateinit var requirementRef: String
+
+    lateinit var newReqVariantRef: String
+
+
+    @Inject
+    lateinit var projectRepository: ProjectRepository
+
+    @Inject
+    lateinit var needRepository: NeedRepository
+
+    @Inject
+    lateinit var requirementRepository: RequirementRepository
+
+    private val mockedProducts: MutableList<Product> = mutableListOf()
+
+    private val mockedPublications: MutableList<Publication> = mutableListOf()
+
+    private val mockedRequirements: MutableList<Requirement> = mutableListOf()
+
+    private val mockedNeeds: MutableList<Need> = mutableListOf()
+
+    private val mockedCodelists: MutableList<Codelist> = mutableListOf()
+
+
+    @BeforeAll
+    @Transactional
+    fun setUp() {
+
+        token = KeycloakAccess.getAccessToken("alice")
+
+        val useThisProject = Project().apply {
+            ref = "testref-123"
+            title = "Prosjekttittel1"
+            description = "Prosjektbeskrivelse1"
+            products = mockedProducts
+            publications = mockedPublications
+            requirements = mockedRequirements
+            needs = mockedNeeds
+            codelist = mockedCodelists
+        }.also {
+            projectRepository.persistAndFlush(it)
+            projectRef = it.ref
+            projectId = it.id
+        }
+
+        val useThisNeed = Need().apply {
+            ref = "ytuiop-rtgsd-235235235-fdsfdsd"
+            title = "Ny need tittel"
+            project = useThisProject
+            requirements = mockedRequirements
+        }.also {
+            needRepository.persistAndFlush(it)
+            needRef = it.ref
+            needId = it.id
+        }
+
+        Requirement().apply {
+            ref = "sadasdasf-rtgsd-235gfdgfdåpskfæpdsa235235-fdsfdsd"
+            title = "Ny req tittel"
+            project = useThisProject
+            need = useThisNeed
+        }.also {
+            requirementRepository.persistAndFlush(it)
+            requirementRef = it.ref
+            requirementId = it.id
+        }
+    }
+
 
     @Test
     @Order(1)
-    fun getRequirementVariant() {
-        given()
-            .auth()
-            .oauth2(token)
-            .`when`()
-            .get("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements/req1b69-edb2-431f-855a-4368e2bcddd1/requirementvariants/rvrv2b69-edb2-431f-855a-4368e2bcddd1")
-            .then()
-            .statusCode(200)
+    fun `create variant then return 201 and verify ref has value`() {
+
+        RestAssured.defaultParser = Parser.JSON
+
+        val rv = RequirementVariantForm().apply {
+            description = "Integrasjonstest rv desc"
+            requirementText = "Integrasjonstest rv reqtext"
+            instruction = "Integrasjonstest rv instruction"
+            useProduct = true
+            useSpecification = true
+            useQualification = true
+        }
+
+        val response =
+            given()
+                .auth()
+                .oauth2(token)
+                .`when`()
+                .body(RequirementVariantForm().toEntity(rv))
+                .header("Content-type", "application/json")
+                .post("/api/v1/projects/$projectRef/needs/$needRef/requirements/$requirementRef/requirementvariants")
+
+        assertEquals(201, response.statusCode)
+        assertTrue(response.headers.hasHeaderWithName("Location"))
+
+
+        val newUrlLocation = response.headers.getValue("Location")
+        val newRefToUse = newUrlLocation.split("requirementvariants/")[1]
+
+        assertTrue(newRefToUse.isNotEmpty())
+
+        newReqVariantRef = newRefToUse
     }
 
-    @Test
+
     @Order(2)
-    fun listRequirementVariants() {
-        given()
+    @Test
+    fun `get variant then return 200 and verify ref`() {
+
+        val response = given()
             .auth()
             .oauth2(token)
             .`when`()
-            .get("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements/req1b69-edb2-431f-855a-4368e2bcddd1/requirementvariants")
-            .then()
-            .statusCode(200)
+            .get("/api/v1/projects/$projectRef/needs/$needRef/requirements/$requirementRef/requirementvariants/$newReqVariantRef")
+
+        assertEquals(200, response.statusCode())
+
+
+        val entity = response.body.jsonPath().getObject("", RequirementVariant::class.java)
+
+        assertEquals(newReqVariantRef, entity.ref)
     }
+
 
     @Test
     @Order(3)
-    fun createRequirementVariant() {
-        RestAssured.defaultParser = Parser.JSON
-        val rv = RequirementVariantForm()
-        rv.description = "Integrasjonstest rv desc"
-        rv.requirementText = "Integrasjonstest rv reqtext"
-        rv.instruction = "Integrasjonstest rv instruction"
-        rv.useProduct = true
-        rv.useSpecification = true
-        rv.useQualification = true
+    fun `list variant then return 200 and verify ref`() {
 
-        given()
+        val response = given()
             .auth()
             .oauth2(token)
             .`when`()
-            .body(rv)
-            .header("Content-type", "application/json")
-            .post("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements/req1b69-edb2-431f-855a-4368e2bcddd1/requirementvariants")
-            .then()
-            .statusCode(201)
+            .get("/api/v1/projects/$projectRef/needs/$needRef/requirements/$requirementRef/requirementvariants/")
+
+        assertEquals(200, response.statusCode)
+
+
+        val list = response.body.jsonPath().getList("", RequirementVariant::class.java)
+        val lastEntityInList = list.last()
+
+
+        assertEquals(newReqVariantRef, lastEntityInList.ref)
     }
+
 
     @Test
     @Order(4)
-    fun updateRequirementVariant() {
+    fun `update variant then return 200 and verify updated attributes`() {
+
         RestAssured.defaultParser = Parser.JSON
-        val rv = RequirementVariantForm()
-        rv.description = "Integrasjonstest rv desc"
-        rv.requirementText = "Integrasjonstest rv reqtext"
-        rv.instruction = "Integrasjonstest rv instruction"
-        rv.useProduct = true
-        rv.useSpecification = true
-        rv.useQualification = true
 
-        given()
+        val update = RequirementVariantForm().apply {
+            description = "Integrasjonstest rv desc"
+            requirementText = "Integrasjonstest rv reqtext"
+            instruction = "Integrasjonstest rv instruction"
+            useProduct = true
+            useSpecification = true
+            useQualification = true
+        }
+
+        val response = given()
             .auth()
             .oauth2(token)
             .`when`()
-            .body(rv)
+            .body(RequirementVariantForm().toEntity(update))
             .header("Content-type", "application/json")
-            .put("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements/req1b69-edb2-431f-855a-4368e2bcddd1/requirementvariants/rvrv2b69-edb2-431f-855a-4368e2bcddd1")
-            .then()
-            .statusCode(200)
+            .put("/api/v1/projects/$projectRef/needs/$needRef/requirements/$requirementRef/requirementvariants/$newReqVariantRef")
+
+        assertEquals(200, response.statusCode)
+
+
+        val entity = response.body.jsonPath().getObject("", RequirementVariant::class.java)
+
+        assertEquals(update.description, entity.description)
+        assertEquals(update.requirementText, entity.requirementText)
+        assertEquals(update.instruction, entity.instruction)
+        assertEquals(update.useProduct, entity.useProduct)
+        assertEquals(update.useSpecification, entity.useSpecification)
+        assertEquals(update.useQualification, entity.useQualification)
     }
 
-    @Test
+
     @Order(5)
-    fun deleteRequirementVariant() {
-        given()
+    @Test
+    fun `delete variant then return 200 and verify deleted ref`() {
+
+        val response = given()
             .auth()
             .oauth2(token)
             .`when`()
-            .delete("/api/v1/projects/aaa4db69-edb2-431f-855a-4368e2bcddd1/requirements/req1b69-edb2-431f-855a-4368e2bcddd1/requirementvariants/rvrv3b69-edb2-431f-855a-4368e2bcddd1")
-            .then()
-            .statusCode(200)
+            .delete("/api/v1/projects/$projectRef/needs/$needRef/requirements/$requirementRef/requirementvariants/$newReqVariantRef")
+
+        assertEquals(200, response.statusCode)
     }
 
+
+    @AfterAll
+    @Transactional
+    fun `delete dependencies`() {
+
+        projectRepository.deleteById(projectId)
+        needRepository.deleteById(needId)
+        requirementRepository.deleteById(requirementId)
+    }
 }
